3fdc3aecf17c34ff862b1f992064645b212cc8ff
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-07-05 14:36:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'bạn';
+const giftUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+const eventName = 'hộp quà bí mật tặng cho';
+const titleCard = 'rick roll';
+const contentCard = 'Never gonna give you up';
+const giftImage = 'rick.png';
+//const base64 = '';
+//const giftImageBase64 = "data:image/png;base64, " + base64;
